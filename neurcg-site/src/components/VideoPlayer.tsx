3657ff1ba6fa39c84f38dedbda595a9937be7068
humanshi123 <155156882+humanshi123@users.noreply.github.com>
2024-09-13 17:12:34 +0530

    Update VideoPlayer.tsx

--- neurcg-site/src/components/VideoPlayer.tsx
+++ neurcg-site/src/components/VideoPlayer.tsx
@@ -16,6 +16,8 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ url }) => {
         url={url}
         // url='/assets/homevideo.mp4'
         width='100%'
+        playing={true}
+        muted={true}
         height='100%'
         controls={true}
       />
